3bbd7d6a81c57108716bb0ea8a91e6ab33d8ac88
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-02-19 14:18:39 -0500

    Add section headings

--- ircurltitlebot/__main__.py
+++ ircurltitlebot/__main__.py
@@ -11,16 +11,19 @@ log = logging.getLogger(__name__)
 
 
 def main() -> None:
+    # Read args
     parser = argparse.ArgumentParser(prog=config.PACKAGE_NAME, description="IRC URL title bot")
     parser.add_argument('--config-path', required=True, help='Configuration file path, e.g. /some/dir/config.json')
     user_config_path = parser.parse_args().config_path
 
+    # Read user config
     log.debug('Reading user configuration file %s', user_config_path)
     with open(user_config_path) as user_config_file:
         user_config = json.load(user_config_file)
     log.info('Read user configuration file "%s" having configuration: %s',
              user_config_path, json.dumps(user_config).replace(user_config['nick_password'], '<password>'))
 
+    # Start bot
     try:
         log.debug('Initializing bot.')
         Bot(user_config).start()
