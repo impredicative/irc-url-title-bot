4800dc3770af17d7b9487e067b135fb31ca7167e
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-02-19 14:20:43 -0500

    Import logging

--- ircurltitlebot/bot.py
+++ ircurltitlebot/bot.py
@@ -1,8 +1,13 @@
+import logging
+from typing import Dict
+
 from . import config
+
+log = logging.getLogger(__name__)
 
 
 class Bot:
-    def __init__(self, user_config):
+    def __init__(self, user_config: Dict):
         self._user_config = user_config
 
     def start(self):
