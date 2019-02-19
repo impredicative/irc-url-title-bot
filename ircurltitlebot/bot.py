234db815c5e4f3d9f95d84d93f1a98201096319e
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-02-19 18:33:08 -0500

    Rename variable from address to channel

--- ircurltitlebot/bot.py
+++ ircurltitlebot/bot.py
@@ -3,10 +3,12 @@ import threading
 from typing import List, NoReturn, Tuple
 
 import miniirc
+from urlextract import URLExtract
 
 from . import config
 
 log = logging.getLogger(__name__)
+url_extractor = URLExtract()
 
 
 class Bot:
@@ -28,14 +30,21 @@ def _handler(irc: miniirc.IRC, hostmask: Tuple[str, str, str], args: List[str]) 
     log.debug('Handling incoming message: num_threads=%s, hostmask=%s, args=%s',
               threading.active_count(), hostmask, args)
     user, _ident, _hostname = hostmask
-    address = args[0]
+    channel = args[0]
     msg = args[-1]
     assert msg.startswith(':')
     msg = msg[1:]
 
-    if address not in config.INSTANCE['channels']:
-        assert address == config.INSTANCE['nick']
+    if channel not in config.INSTANCE['channels']:
+        assert channel == config.INSTANCE['nick']
         log.info('Ignoring incoming private message from %s: %s.', user, msg)
         return
 
-    irc.msg(address, f'{config.TITLE_PREFIX} {msg}')
+    try:
+        urls = url_extractor.find_urls(msg, only_unique=True)
+    except Exception as exc:
+        log.error('Error extracting URLs in message from %s in %s having content "%s". The error is: %s',
+                  user, channel, msg, exc)
+        return
+
+    irc.msg(channel, f'{config.TITLE_PREFIX} {msg}')
