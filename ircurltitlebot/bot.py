d04c6a0f3ddf610073ee8f184a92ed495592d31f
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-02-20 19:21:26 -0500

    Correctly log result timeout

--- ircurltitlebot/bot.py
+++ ircurltitlebot/bot.py
@@ -81,7 +81,7 @@ def _handle_titles(channel: str) -> NoReturn:
         try:
             result = url_future.result(timeout=title_timeout)
         except concurrent.futures.TimeoutError:
-            log.error('Result timed out after %.1fs given a timeout of %.1fs.', monotonic() - start_time)
+            log.error('Result timed out after %.1fs.', monotonic() - start_time)
         else:
             if result is None:
                 continue
