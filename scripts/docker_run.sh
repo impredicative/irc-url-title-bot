2eb01d12f77487f9cf45a05ddf2e133bfa724758
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-02-23 18:59:26 -0500

    Update config paths

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run --restart always -v /home/devuser/PycharmProjects/utils/config/irc-url-title-bot/dev:/config:ro irc-url-title-bot
+docker run --restart always -v /home/devuser/PycharmProjects/freenode-bots/irc-url-title-bot/dev:/config:ro irc-url-title-bot
