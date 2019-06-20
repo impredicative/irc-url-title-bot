c910e193fef9e199b192904a4de96fad6d382774
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-06-19 21:30:24 -0400

    Update miniirc to 1.4.0, and explicitly specify colon=True for future-proofing for miniirc 2.x

--- ircurltitlebot/bot.py
+++ ircurltitlebot/bot.py
@@ -146,14 +146,14 @@ def _get_title(irc: miniirc.IRC, channel: str, user: str, url: str) -> Optional[
 # Ref: https://tools.ietf.org/html/rfc1459
 
 
-@miniirc.Handler(900)
+@miniirc.Handler(900, colon=True)
 def _handle_loggedin(_irc: miniirc.IRC, hostmask: Tuple[str, str, str], args: List[str]) -> None:
     log.debug('Handling RPL_LOGGEDIN (900): hostmask=%s, args=%s', hostmask, args)
     identity = args[1]
     log.info('Client identity as <nick>!<user>@<host> is %s.', identity)
 
 
-@miniirc.Handler('PRIVMSG')
+@miniirc.Handler('PRIVMSG', colon=True)
 def _handle_privmsg(irc: miniirc.IRC, hostmask: Tuple[str, str, str], args: List[str]) -> None:
     # Parse message
     log.debug('Handling incoming message: hostmask=%s, args=%s', hostmask, args)
