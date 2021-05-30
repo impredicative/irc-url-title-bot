ec195592e11616040b87f9c5a421c7f61163c8d0
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2021-05-30 19:30:38 -0400

    Make parsed URL validation more readable

--- ircurltitlebot/util/urllib.py
+++ ircurltitlebot/util/urllib.py
@@ -8,4 +8,7 @@ _LOCALHOST_NAMES = {"127.0.0.1", "localhost", "ip6-localhost", "ip6-loopback", "
 def validate_parsed_url(url: str) -> bool:
     """Return whether a URL is valid after parsing it."""
     parsed = urlparse(url.casefold())
-    return not ((parsed.scheme not in ("http", "https", "")) or (parsed.netloc in _LOCALHOST_NAMES) or ((parsed.netloc == "") and (parsed.path in _LOCALHOST_NAMES)))
+    is_scheme_valid = parsed.scheme in ("http", "https", "")
+    is_netloc_valid = parsed.netloc not in _LOCALHOST_NAMES
+    is_path_valid = (parsed.path not in _LOCALHOST_NAMES) if (parsed.netloc == "") else True
+    return is_scheme_valid and is_netloc_valid and is_path_valid
